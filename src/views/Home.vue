b9e548b92abe5ebb761181f5201ef1763bc4b70a
roi.ilan <roiilan23@gmail.com>
2020-05-17 20:05:07 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav-bar />
+  </div>
+</template>
+
+<script>
+import navBar from '../components/nav-bar'
+
+export default {
+  name: 'Home',
+  components: {
+    navBar
+  }
+}
+</script>
