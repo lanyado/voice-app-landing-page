453754476fb660c49c15dd520319635d3706a253
roi.ilan <roiilan23@gmail.com>
2020-05-17 20:18:10 +0300

    create vue project+ basic nav bar

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-bar-container">
+    <nav class="nav-bar width-container flex">
+
+<div class="links">
+    <router-link to="" >
+          <span>צור קשר</span>
+    </router-link>
+    <router-link to="" >
+          <span>אודות</span>
+    </router-link>
+    <router-link to="" title="change language">
+          <span>Eng</span>
+    </router-link>
+
+</div>
+
+
+      <router-link to="/">
+        <img class="logo" src="" alt="Logo" />
+      </router-link>
+      
+      
+        <div class="spnoserd-container">
+          <span>אנו נתמכים על ידי</span>
+          <img src="" alt="mobileye">
+          <img src="" alt="Intel">
+          <img src="" alt="Weizmann Institute of Science">
+        </div>
+
+        
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+data() {
+    return {   
+
+    };
+  },
+};
+</script>
+
