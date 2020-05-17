894ac9f68c4c18eba821b48ea5cad69139d95651
roi.ilan <roiilan23@gmail.com>
2020-05-17 22:55:30 +0300

    basic footer+hompage

---
+++ src/components/main-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    footer!!!
+  </div>
+</template>
+
+<script>
+
+export default {
+data() {
+    return {   
+
+    };
+  },
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,74 @@
 <template>
   <div>
     <nav-bar />
+
+<div class="welcome-container">
+<span>הקול שלך יכול לנצח את הקורונה</span>
+
+</div>
+
+
+<div class="illustration-container">
+
+
+</div>
+
+
+<div class="explanation-container">
+
+</div>
+
+
+<div class="actions-container">
+  
+</div>
+
+
+
+<div class="sponsors-container">
+
+</div>
+
+
+<div class="members-container">
+<ul class="special-members">
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+</ul>
+
+<ul class="members-names">
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+  <li> </li>
+
+</ul>
+</div>
+
+<main-footer/>
+
   </div>
 </template>
 
 <script>
 import navBar from '../components/nav-bar'
+import mainFooter from '../components/main-footer'
 
 export default {
   name: 'Home',
   components: {
-    navBar
+    navBar,
+    mainFooter
   }
 }
 </script>
