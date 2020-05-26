218800a879c887a3434ba75d791454810e17e91f
lanyado <lanyado98@gmail.com>
2020-05-26 18:24:50 +0200

    continued

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="nav-bar-container">
-    <header>
+    <header :class="{ scrolled: scrolled }">
     <label for="toggle-1" class="toggle-menu"><ul><li></li> <li></li> <li></li></ul></label>
     <input type="checkbox" id="toggle-1">
 
     <nav class="nav-bar width-container flex">
       <ul class="links">
-        <li>
-          <router-link :to="`/${$i18n.locale}/contact`">
-            <span>{{$t('nav_bar.contact')}}</span>
-          </router-link>
-        </li>
-        <li>
-          <router-link to>
-            <span>{{$t('nav_bar.about')}}</span>
-          </router-link>
-        </li>
+
         <li>
         <router-link to>
           <span title="change language" @click="setLang()">{{$t('nav_bar.changeLang')}}</span>
@@ -31,8 +22,18 @@
         </div>-->
         </router-link>
         </li>
+                <li>
+          <router-link :to="`/${$i18n.locale}/contact`">
+            <span>{{$t('nav_bar.contact')}}</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to>
+            <span>{{$t('nav_bar.about')}}</span>
+          </router-link>
+        </li>
       </ul>
-      <img id="logo" src="../assets/svg/logo_with_name.svg" alt="Logo" />
+      <img id="logo" :src="logoUrl" alt="Logo" />
 
       <div class="spnoserd-container">
         <span>{{$t('nav_bar.spnoserd_by')}}</span>
@@ -53,9 +54,12 @@ import navBarStyles from '../design/components/nav-bar.css';
 export default {
   data() {
     return {
+        logoUrl: '../assets/svg/logo_with_name_white.svg',
+        scrolled: false,
     };
   },
  created() {
+   window.addEventListener('scroll', this.handleScroll);
     //  if(this.$route.params.lang==='en'){
     // console.log('en')
     // }
@@ -76,17 +80,18 @@ export default {
         default:
         // code block
       }
-    },   
+    },  
+    handleScroll (event) {
+      if ((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0 ) >= 100 ) {
+          this.scrolled = true;
+          this.logoUrl = '../assets/svg/logo_with_name_blue.svg';
+      } 
+      else {
+          this.scrolled = false;
+          this.logoUrl = '../assets/svg/logo_with_name_white.svg';
+      }
+    }, 
   }
-};
-window.onscroll = function () { 
-    const header = document.getElementsByTagName('header')[0];
-    if ((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0 ) >= 100 ) {
-        header.classList.add("scrolled");
-    } 
-    else {
-        header.classList.remove("scrolled");
-    }
 };
 </script>
 
