dfb8fc80bbbf8070d0625262b4429878fa2e2f82
lanyado <lanyado98@gmail.com>
2020-06-05 00:38:50 +0200

    removed unused libraries

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -40,14 +40,6 @@ import navBar from "../components/nav-bar";
 import contactStyle from "../design/components/contact.scss";
 import Swal from 'sweetalert2'
 
-import Vue from 'vue';
-import VueSweetalert2 from 'vue-sweetalert2';
- 
-// If you don't need the styles, do not connect
-import 'sweetalert2/dist/sweetalert2.min.css';
- 
-Vue.use(VueSweetalert2);
-
 export default {
   data() {
     return {
