c7436a8055778c213e3a2eeca8ff6e1ef854c543
roi.ilan <roiilan23@gmail.com>
2020-06-07 02:25:57 +0300

    nav bar funcs

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -26,12 +26,49 @@
       <router-link :to="`/${$i18n.locale}/`" >
         <img id="logo" :src="logoUrl" alt="Logo"/>
       </router-link>
-      <div class="spnoserd-container">
+
+      <div v-if="$route.name==='contact'" class="funcs">
+        
+        <button @click="openShareDialog()" id="share-dialog-button">
+          <img src="../assets/svg/share.svg" alt="share icon"/>
+          {{ $t('actions-container.card2.share_btn') }}      
+        </button>
+        <div id="share-dialog" class="share-dialog">
+          <span> {{ $t('actions-container.card2.share_dialog') }}       </span>
+          <ul>
+            <li>
+              <a :href="whatsappLink" data-action="share/whatsapp/share" target="_blank">
+                <img src="../assets/svg/whatsapp.svg" alt="whatsapp icon"/>
+              </a>
+            </li>
+            <li>
+              <a :href="telegramLink" target="_blank" >
+                <img src="../assets/svg/telegram.svg" alt="telegram icon"/>
+              </a>
+            </li>
+            <li>
+            <a :href="facebookLink" target="_blank">
+              <img src="../assets/svg/facebook.svg" alt="facebook icon"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a href="##" class="close-popup"></a>
+
+        <button class="record-btn" @click="open(appLink)">
+        <img src="../assets/svg/blue-circle.svg" alt="white circle"/>
+        {{ $t('nav_bar.record')}}
+        </button>        
+      </div>
+
+
+      <div v-else class="spnoserd-container">
         <span>{{$t('nav_bar.spnoserd_by')}}</span>
         <img src="../assets/png/mobileye-logo.png" alt="mobileye" />
         <img src="../assets/png/intel-logo.png" alt="Intel" />
         <img src="../assets/png/weizman-logo.png" alt="Weizmann Institute of Science" />
       </div>
+
     </nav>
     </header>
   </div>
@@ -60,12 +97,23 @@ export default {
           }
         },
         scrolled: false,
+       isContactPage: false
     };
   },
   computed: {
-      logoUrl: function () {
+        logoUrl: function () {
         return this.scrolled||this.static ? this.logos['scrolled'][this.language] : this.logos['default'][this.language]
-    }
+    },
+    appLink: function () {
+      switch (this.language) {
+        case "he":
+          return "https://corona.voca.ai/he/login";
+          break;
+        case "en":
+          return "https://corona.voca.ai/en/login";
+          break;
+      }
+  },
   },
   props: {
     homePage: {
@@ -87,8 +135,24 @@ export default {
  created() {
     this.switchLanguage(this.language)
     if (!this.static) window.addEventListener('scroll', this.handleScroll);
+
+    // if (this.$route.path === "/:lang/contact"){
+    //   this.isContactPage = true;
+    //   console.log('contatct page')
+    // }
+    // else{
+    //   console.log('NOOOOOO contatct page')
+    // }
+    // console.log(this.$route.path, " this.$route.path")
   },
   methods: {
+    open: link => {
+      window.open(link);
+    },
+    openShareDialog: () => {
+      location.href ="#share-dialog";
+      //document.getElementById('share-button').click();
+    },
     setLang() {
       switch (this.language) {
         case "he":
